ab9e0b32e438151f94f801061beb92c3cd7394a3
Sai Charan <saicharankadiyala@gmail.com>
2025-02-25 17:01:11 +0530

    tried to fix movie detail api error

--- src/app/api/movies/details/[id]/route.ts
+++ src/app/api/movies/details/[id]/route.ts
@@ -1,25 +1,25 @@
-import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import axios from "axios";
 import { authenticateJWT } from "@/helper/common-auth";
 
 export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const source = await request.headers.get("request");
     if (source == "mobile") {
       await authenticateJWT(request);
     }
-    const id = params.id;
+    const id = (await params).id;
     const result = await axios.get(
       `https://api.themoviedb.org/3/movie/${id}?api_key=500a2293d6403e9c3caedb4591ae7624`
     );
 
-    return NextResponse.json({ data: result.data, success: true });
+    return Response.json({ data: result.data, success: true });
   } catch (error) {
     if (error instanceof Error) {
-      return NextResponse.json({ error: error.message, success: false });
+      return Response.json({ error: error.message, success: false });
     }
   }
 }
